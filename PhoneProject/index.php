920f7016d4ac5a239486fdf0f37c322058e6b259
SharmileeS <sharmilee.sivakumaran@gmail.com>
2015-11-28 13:04:07 -0800

    Specs tab
    Adding phone specs with specs table, image and more info links. Empty
    values with N/A.

--- PhoneProject/index.php
+++ PhoneProject/index.php
@@ -9,6 +9,7 @@
 		<link rel="stylesheet" href="css/live_search_style.css" type="text/css">
 		<link rel="stylesheet" href="css/jquery-ui.min.css" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.1.0/bootstrap.min.js"></script>
 		<script src="js/jquery-ui.min.js"></script>
 		<script src="js/live_search.js"></script>
 	</head>
@@ -18,16 +19,22 @@
 				<h3>Type in phone model below</h3>
 				<form class="navbar-form navbar-left" role="search" method="post" action="" >
 				    <div class="form-group">
-				        <input type="text" name="search_text" id="searchid"class="form-control" placeholder="Search for phone model">
+				        <input type="text" name="search_text" value="<?=isset($_POST['search_text']) ? $_POST['search_text'] : ''?>" id="searchid" class="form-control" placeholder="Search for phone model">
 				    </div>
 				    <button type="submit" id="search_button" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
 				</form>
 			</div>
-			<div class="row">
-				<div class="col-sm-4">
-					
-				</div>
+			<ul class="nav nav-pills">
+				<li class="active"><a href="#Specs" data-toggle="tab">Specifications</a></li>
+				<li><a href="#trshoot" data-toggle="tab">Troubleshooting</a></li>
+				<li><a href="#resale" data-toggle="tab">Resale</a></li>
+			</ul>
+			<div class="tab-content" id="tabs">
+				<div class="tab-pane active" id="Specs"><?php include 'search_phone_results.php'?></div>
+				<div class="tab-pane" id="trshoot">Coming soon...</div>
+				<div class="tab-pane" id="resale">Coming soon...</div>
 			</div>
+		</div>
 		</div>
 	</body>
 </html>
--- PhoneProject/search_phone_results.php
+++ PhoneProject/search_phone_results.php
@@ -1,34 +1,62 @@
 <?php require_once 'dbconfig.php';
 
 if(isset($_POST['search_text']) && !empty($_POST['search_text']) ){
-	$data = array();
-	$result = mysqli_query($con,"SELECT phone_model FROM phonescoop where phone_model LIKE '%".$_POST['search_text']."%'");	
+	$result = mysqli_query($con,"SELECT * FROM phonescoop where phone_model LIKE '%".$_POST['search_text']."%'");
+	$img_url=null;
+	$site_url=null;
+	$model=null;
 ?>
-
-	<h4>Search results:</h4>
-	<table class="table table-bordered table-condensed table-hover">
-		<thead>
-			<tr>
-				<th>Phones</th>
-			</tr>
-		</thead>
-			<tbody class="tbody">
-
-<?php 
-	while ($row = mysqli_fetch_array($result)) {
-		array_push($data, $row['phone_model']);	
-?>		
+<table class="table">
+	<tbody class="tbody">
 		<tr>
-			<td>			
-				<?php echo htmlspecialchars($row['phone_model']); $flag=1; ?>	
-			</td> 
-		</tr> 
-	
-<?php	
-	}	
-?>			</tbody> 
-	</table>		
+			<td>
+			<table class="table table-bordered table-condensed">
+				<tbody class="tbody">
 
+	<?php 
+		while ($row = mysqli_fetch_assoc($result)) {
+			foreach($row as $key => $value) {
+				if ($key == 'image') {
+					$img_url=$value;
+					continue;
+				} elseif ($key == 'url') { 
+					$site_url=$value;
+					continue;
+				} elseif ($key == 'phone_model') {
+					$model=$value;
+					continue;
+				}
+	?>		
+				<tr>
+					<td>			
+						<span style="font-weight:bold"><?= $key ?></span>
+					</td> 
+					<?php if($value == '') { ?>
+					<td>			
+						<?= 'N/A' ?>	
+					</td> 
+					<?php } else { ?>
+					<td>			
+						<?= htmlspecialchars($value) ?>	
+					</td> 
+					<?php } ?>
+				</tr> 
+		
+	<?php	
+			 }
+		}	
+	?>			</tbody> 
+			</table>	
+			</td>
+			<td>
+				<h4><?= $model ?></h4>
+				<img src="<?= $img_url ?>" width="304" height="225"/>
+				<p>For more info, <a href="<?= $site_url ?>">click here</a></p>
+			</td>
+		</tr>
+	</tbody>
+</table>
+	
 <?php	
 }	
 ?>		
